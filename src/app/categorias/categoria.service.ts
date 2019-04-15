96ed2b613a837f967835b296661e1ab341bc3e89
Aluno <aluno@eteclencois.com.br>
2019-04-15 11:44:30 -0300

    n lebro mas 15/04/2019

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -1,12 +1,42 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+export class CategoriaFiltro {
+  nome: string;
+  pagina = 0;
+  itensporpagina = 2;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor(private hhtp: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  categoriasURI='http://localhost:8080/categorias';
+  categoriasUrl = 'http://localhost:8080/categorias';
+
+  pesquisar(filtro: CategoriaFiltro): Promise<any> {
+
+    let params = new HttpParams({
+      fromObject: {
+        page: filtro.pagina.toString(),
+        size: filtro.itensporpagina.toString()
+      }
+    });
+
+    if (filtro.nome) {
+      params = params.append('nome', filtro.nome)
+    }
+    return this.http.get<any>(`${this.categoriasUrl}`, { params })
+      .toPromise()
+      .then(response => {
+        const categorias = response.content;
+        const resultado = {
+          categorias,
+          total: response.totalElemtents
+        }
+        return resultado;
+      });
+  }
 }
