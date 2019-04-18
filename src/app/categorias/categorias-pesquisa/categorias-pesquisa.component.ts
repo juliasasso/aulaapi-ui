f0a17db759ccc5f4733abb54a8606ecadc92ae45
Aluno <aluno@eteclencois.com.br>
2019-04-18 12:22:00 -0300

    18/04/2019

--- src/app/categorias/categorias-pesquisa/categorias-pesquisa.component.ts
+++ src/app/categorias/categorias-pesquisa/categorias-pesquisa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaFiltro, CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categorias-pesquisa',
@@ -6,16 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorias-pesquisa.component.css']
 })
 export class CategoriasPesquisaComponent implements OnInit {
-  
-  categorias =  [
-    {id: '1', nome:'Informatica'},
-    {id: '1', nome:'Informatica'},
-    {id: '1', nome:'Informatica'}
-    
-  ];
-  constructor() { }
+
+  totalRegistros = 0;
+  filtro = new CategoriaFiltro();
+  categorias = [];
+  constructor(private categoriaService: CategoriaService) { }
 
   ngOnInit() {
+    this.pesquisar();
+  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.categoriaService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.categorias = resultado.categorias;
+      });
   }
 
 }
